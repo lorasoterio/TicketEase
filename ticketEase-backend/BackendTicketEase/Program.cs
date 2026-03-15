40557465e1d06927af670a020022f5e38fcb3f0f
lorasoterio <lorasoterio247@gmail.com>
2026-03-15 16:11:53 +0800

    Update Program.cs

--- ticketEase-backend/BackendTicketEase/Program.cs
+++ ticketEase-backend/BackendTicketEase/Program.cs
@@ -1,8 +1,17 @@
+using BackendTicketEase.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// This line reads your connection string and registers the DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
