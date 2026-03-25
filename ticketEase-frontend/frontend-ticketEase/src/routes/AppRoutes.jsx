07ef3a403dbfe39584366b76dc48a1613a038592
lorasoterio <lorasoterio247@gmail.com>
2026-03-25 17:12:21 +0800

    Update AppRoutes.jsx
    added the routes for the login and registration page

--- ticketEase-frontend/frontend-ticketEase/src/routes/AppRoutes.jsx
+++ ticketEase-frontend/frontend-ticketEase/src/routes/AppRoutes.jsx
@@ -1,14 +1,15 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import DocumentRequestPage from '../pages/user/RequestTicket';
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import DocumentRequestPage from "../pages/user/RequestTicket";
+import LoginPage from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
 
 export default function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={<Navigate to="/request-document" />} />
-            <Route path="/request-document" element={<DocumentRequestPage />} />
-
-            {/* Add more routes here as you create new pages */}
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/request-document" element={<DocumentRequestPage />} />
+    </Routes>
+  );
 }
