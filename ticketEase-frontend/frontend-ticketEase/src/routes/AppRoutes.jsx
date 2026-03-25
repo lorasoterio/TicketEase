c0ca56bac83608c98194b49cc63b6c840a572fc8
lorasoterio <lorasoterio247@gmail.com>
2026-03-25 16:19:40 +0800

    Update AppRoutes.jsx
    remove the path for general inquiry because it was already deleted

--- ticketEase-frontend/frontend-ticketEase/src/routes/AppRoutes.jsx
+++ ticketEase-frontend/frontend-ticketEase/src/routes/AppRoutes.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import DocumentRequestPage from '../pages/user/DocumentRequestPage';
-import GeneralInquiryPage from '../pages/user/GeneralInquiryPage';
+import DocumentRequestPage from '../pages/user/RequestTicket';
+
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Navigate to="/request-document" />} />
             <Route path="/request-document" element={<DocumentRequestPage />} />
-            <Route path="/general-inquiry" element={<GeneralInquiryPage />} />
+
             {/* Add more routes here as you create new pages */}
         </Routes>
     );
