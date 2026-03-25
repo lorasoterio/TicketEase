b1355824eb6119997cb847cc2c636b5ce5f9c316
lorasoterio <lorasoterio247@gmail.com>
2026-03-25 16:15:24 +0800

    remove files

--- ticketEase-backend/BackendTicketEase/Models/InquiryRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace BackendTicketEase.Models;
-
-public class InquiryRequest
-{
-    public int InquiryRequestId { get; set; }
-    public int TicketId { get; set; }
-    public string InquiryDescription { get; set; } = string.Empty;
-}
