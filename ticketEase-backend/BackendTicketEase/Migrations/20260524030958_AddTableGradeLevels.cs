8c0984db92b739d350c40c3c3bbc7024665ed1b0
lorasoterio <lorasoterio@mabinicolleges.edu.ph>
2026-05-24 11:16:53 +0800

    feat: Add GradeLevels table and update AppDbContext model snapshot

---
+++ ticketEase-backend/BackendTicketEase/Migrations/20260524030958_AddTableGradeLevels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BackendTicketEase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableGradeLevels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GradeLevels",
+                columns: table => new
+                {
+                    GradeLevelId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GradeLevelName = table.Column<string>(type: "text", nullable: false),
+                    LevelOrder = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GradeLevels", x => x.GradeLevelId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GradeLevels_GradeLevelName",
+                table: "GradeLevels",
+                column: "GradeLevelName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GradeLevels");
+        }
+    }
+}
