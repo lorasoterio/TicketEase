a0b7b840cdaa078e7c5776e27fac048ad8f17ccc
lorasoterio <lorasoterio@mabinicolleges.edu.ph>
2026-05-24 11:26:20 +0800

    feat: Add Strand model and corresponding database migration

---
+++ ticketEase-backend/BackendTicketEase/Migrations/20260524032447_AddTableStrand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BackendTicketEase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableStrand : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Strands",
+                columns: table => new
+                {
+                    StrandId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StrandCode = table.Column<string>(type: "text", nullable: false),
+                    StrandName = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Strands", x => x.StrandId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Strands");
+        }
+    }
+}
